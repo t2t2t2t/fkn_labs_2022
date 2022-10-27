dfb43d958f4219d28367fc2f68c3aca75389db97
Т <4@4.com>
2022-10-27 23:18:38 +0600

    LAb 2

--- app/src/main/java/com/example/marvel_app_android/ui/theme/Color.kt
+++ app/src/main/java/com/example/marvel_app_android/ui/theme/Color.kt
@@ -2,8 +2,7 @@ package com.example.marvel_app_android.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val DS = Color(0xFF602325)
-val SM = Color(0xFF0B0E17)
-val IM = Color(0xFF510412)
-val TH = Color(0xFF3D464E)
-val BW = Color(0xFF2A3436)
+val Purple200 = Color(0xFFBB86FC)
+val Purple500 = Color(0xFF6200EE)
+val Purple700 = Color(0xFF3700B3)
+val Teal200 = Color(0xFF03DAC5)
