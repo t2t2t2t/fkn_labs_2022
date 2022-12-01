006f11c9875a77c22b3200d8859613311cfc755a
Т <4@4.com>
2022-12-01 20:48:47 +0600

    Lab3

--- app/src/main/java/com/example/marvel_app_android/MainActivity.kt
+++ app/src/main/java/com/example/marvel_app_android/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.marvel_app_android
 
 import android.os.Bundle
+import android.os.StrictMode
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.navigation.compose.rememberNavController
@@ -11,11 +12,14 @@ import com.example.marvel_app_android.ui.theme.Marvel_App_AndroidTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val policy = StrictMode.ThreadPolicy.Builder().permitAll().build()
+        StrictMode.setThreadPolicy(policy)
         setContent {
             Marvel_App_AndroidTheme {
                 val navController = rememberNavController()
                 NavigationAppHost(navController = navController)
             }
+
         }
     }
 }
